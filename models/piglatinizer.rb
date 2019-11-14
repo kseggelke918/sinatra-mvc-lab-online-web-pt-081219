13a50ef4732277e4113d5f6846dc233d795ed1da
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-14 22:53:27 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,7 +8,7 @@ class PigLatinizer
     word = text.split("")
     binding.pry
     if word.first == /[aeiou]/
-      word << "way"
+      (word << "way").join 
     else 
       "#{word}ay"
     end 
