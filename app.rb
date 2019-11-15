241928a1e328da1d92fb338c2e59eb8a40bec009
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-15 00:18:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,6 @@ class App < Sinatra::Base
   post '/piglatinize' do 
     @text = params[:user_phrase]
     @piglatinizer = PigLatinizer.new 
-    @piglatinizer.piglatinize(@text)
     erb :piglatinize  
   end 
   
